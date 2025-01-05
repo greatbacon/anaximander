a537fb1f0e40f97e7e443e0dee328ee8809a7239
greatbacon <hamiltonjreed@gmail.com>
2025-01-05 14:29:34 -0700

    Add sveaflet and initial mapbox

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,22 @@
-<script>
-	import { page } from "$app/stores";    
+<script lang="ts">
+	import { Map, TileLayer, Marker, Popup } from 'sveaflet';	
 
-	//TODO install and configure MapBox w/ OpenStreetMap dataset
-	//Set up layer tool for Denver precinct boundaries, neighborhood boundaries, zip codes, state house & state senate
 </script>
 
+<div class="map">
+	<Map
+		options={{
+			center: [39.769237, -104.986703],
+			zoom: 10.5			
+		}}
+	>
+		<TileLayer url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'} />
+	</Map>
+</div>
+
+<style>
+	.map {
+		width:100%;
+		height:55em;		
+	}
+</style>
